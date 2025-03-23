fd308aaa4bae4770524b2c78aa8efccccf390ca9
yzp <yaozongpu@163.com>
2025-03-23 21:33:29 +0800

    增加基本图形示例

--- OCCT_Test/OCCT_Test.cpp
+++ OCCT_Test/OCCT_Test.cpp
@@ -1,6 +1,7 @@
 #include "OCCT_Test.h"
 #include <QMdiArea>
 #include "MakeBottle.h"
+#include <TColgp_Array1OfPnt.hxx>
 OCCT_Test::OCCT_Test(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -9,8 +10,13 @@ OCCT_Test::OCCT_Test(QWidget *parent)
     this->myDocument2d = new DocumentCommon(this);
     myGeomWidget = new GeomWidget(myDocument3d, myDocument2d, this->ui.widget_3d);
     ui.horizontalLayout_3d->addWidget(myGeomWidget);
-    this->AppendBottle();
-    this->myDocument3d->SetObjects(myObject3d);
+    /*this->AppendBottle();
+    this->myDocument3d->SetObjects(myObject3d);*/
+    //MakeAndShowVertices();
+    //MakeAndShowLine();
+    //MakeAndShowPlane();
+    //MakeAndShowBox();
+    MakeAndShowCurve();
 }
 
 OCCT_Test::~OCCT_Test()
@@ -24,3 +30,72 @@ void OCCT_Test::AppendBottle()
     Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
     myObject3d.Append(aViewCube);
 }
+
+void OCCT_Test::MakeAndShowPoint()
+{
+    gp_Pnt aPnt(100, 1, 1);
+    TopoDS_Vertex aVertex = BRepBuilderAPI_MakeVertex(aPnt);
+    Handle(AIS_InteractiveObject) aShape = new AIS_Shape(aVertex);
+    myObject3d.Append(aShape);
+    Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
+    myObject3d.Append(aViewCube);
+    myDocument3d->SetObjects(myObject3d);
+}
+
+void OCCT_Test::MakeAndShowLine()
+{
+    gp_Pnt aPnt(0, 0, 0);
+    gp_Pnt aPnt2(100, 100, 100);
+    TopoDS_Edge edge = BRepBuilderAPI_MakeEdge(aPnt,aPnt2);
+    Handle(AIS_InteractiveObject) aShape = new AIS_Shape(edge);
+    myObject3d.Append(aShape);
+    Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
+    myObject3d.Append(aViewCube);
+    myDocument3d->SetObjects(myObject3d);
+}
+
+void OCCT_Test::MakeAndShowPlane()
+{
+    gp_Pnt aPnt(0, 0, 0);
+    gp_Dir aDir(0, 0, 1);
+    gp_Pln aPln(aPnt, aDir);
+    Standard_Real umin=0, umax=100, vmin=0, vmax=100;
+    TopoDS_Face face = BRepBuilderAPI_MakeFace(aPln,umin,umax,vmin,vmax);
+    Handle(AIS_InteractiveObject) aShape = new AIS_Shape(face);
+    myObject3d.Append(aShape);
+    Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
+    myObject3d.Append(aViewCube);
+    myDocument3d->SetObjects(myObject3d);
+}
+
+void OCCT_Test::MakeAndShowBox()
+{
+    Standard_Real x = 50, y = 100, z = 200;
+    TopoDS_Solid aBox = BRepPrimAPI_MakeBox(x, y, z);
+    Handle(AIS_InteractiveObject) aShape = new AIS_Shape(aBox);
+    myObject3d.Append(aShape);
+    Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
+    myObject3d.Append(aViewCube);
+    myDocument3d->SetObjects(myObject3d);
+}
+
+void OCCT_Test::MakeAndShowCurve()
+{
+    TColgp_Array1OfPnt* aPnts = new TColgp_Array1OfPnt(1, 4);
+    aPnts->SetValue(1, gp_Pnt(0, 0, 0));
+    aPnts->SetValue(2, gp_Pnt(100, 0, 0));
+    aPnts->SetValue(3, gp_Pnt(300, 500, 0));
+    TColStd_Array1OfReal* aWights = new TColStd_Array1OfReal(1, 4);
+    aWights->SetValue(1, 1);
+    aWights->SetValue(2, 3);
+    aWights->SetValue(3, 4);
+    Handle(Geom_BezierCurve) aCurve = new Geom_BezierCurve(*aPnts, *aWights);
+    TopoDS_Edge edge = BRepBuilderAPI_MakeEdge(aCurve);
+    Handle(AIS_InteractiveObject) aShape = new AIS_Shape(edge);
+    myObject3d.Append(aShape);
+    Handle(AIS_ViewCube) aViewCube = new AIS_ViewCube();
+    myObject3d.Append(aViewCube);
+    myDocument3d->SetObjects(myObject3d);
+}
+
+
--- OCCT_Test/OCCT_Test.h
+++ OCCT_Test/OCCT_Test.h
@@ -7,6 +7,15 @@
 #include <V3d_Viewer.hxx>
 #include <AIS_InteractiveContext.hxx>
 #include <V3d_View.hxx>
+#include <BRepBuilderAPI_MakeVertex.hxx>
+#include <BRepBuilderAPI_MakeEdge.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <TopoDS_Vertex.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Face.hxx>
+#include <TopoDS_Solid.hxx>
+#include <Geom_BezierCurve.hxx>
 #include "GeomWidget.h"
 #include "DocumentCommon.h"
 #include "ui_OCCT_Test.h"
@@ -26,4 +35,15 @@ private:
     DocumentCommon* myDocument3d;
     DocumentCommon* myDocument2d;
     NCollection_Vector<Handle(AIS_InteractiveObject)> myObject3d;
+private:
+    // 创建并显示顶点
+    void MakeAndShowPoint();
+    // 创建并显示线段
+    void MakeAndShowLine();
+    // 创建并显示平面
+    void MakeAndShowPlane();
+    // 创建并显示长方体
+    void MakeAndShowBox();
+    // 创建并显示曲线
+    void MakeAndShowCurve();
 };
